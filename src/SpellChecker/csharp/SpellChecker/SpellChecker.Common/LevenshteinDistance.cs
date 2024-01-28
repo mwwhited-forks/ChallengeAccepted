5c79f6aacb7b2f5bfd4055244550ba6f4cafe7e2
Matthew Whited <matt@whited.us>
2024-01-28 00:14:22 -0500

    stub out

--- src/SpellChecker/csharp/SpellChecker/SpellChecker.Common/LevenshteinDistance.cs
+++ src/SpellChecker/csharp/SpellChecker/SpellChecker.Common/LevenshteinDistance.cs
@@ -1,16 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SpellChecker.Common;
-///
-public class LevenshteinDistance
+
+public static class LevenshteinDistance
 {
-    public LevenshteinDistance()
-    {
-        
-    }    
+    public static int Calculate<T>(ReadOnlySpan<T> left, ReadOnlySpan<T> right) where T : IComparable =>
+        left.Length == 0 ? right.Length :
+        right.Length == 0 ? left.Length :
+        left[0].CompareTo(right[0]) == 0 ? Calculate(left[1..], right[1..]) :
+        1 + new[]{
+            Calculate(left[1..], right),
+            Calculate(left, right[1..]),
+            Calculate(left[1..], right[1..]),
+            }.Min();
 }
+
---
+++ src/SpellChecker/csharp/SpellChecker/SpellChecker.Tests/LevenshteinDistanceTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpellChecker.Common;
+
+namespace SpellChecker.Tests;
+
+[TestClass]
+public class LevenshteinDistanceTests
+{
+    public required TestContext TestContext { get; set; }
+
+    [DataTestMethod, TestCategory("Unit")]
+    [DataRow("can", "can", 0)]
+    [DataRow("cat", "can", 1)]
+    [DataRow("can", "fat", 2)]
+    [DataRow("can", "fater", 4)]
+    public void CalculateTest(string left, string right, int expectedDistance)
+    {
+        var result = LevenshteinDistance.Calculate<char>(left, right);
+        TestContext.WriteLine($"\"{left}\"->\"{right}\" Levenshtein distance:  {result}");
+        Assert.AreEqual(expectedDistance, result);
+    }
+}
