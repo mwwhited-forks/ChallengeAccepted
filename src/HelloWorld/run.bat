59ad7b2cdab43bafc68103af2fee5d4cc38b1f39
Matthew Whited <matt@whited.us>
2024-02-05 18:36:38 -0500

    update examples

--- src/HelloWorld/run.bat
+++ src/HelloWorld/run.bat
@@ -2,36 +2,36 @@
 
 PUSHD rust
 ECHO '-- hello_world.rs'
-rustc hello_world.rs -o hello_world.exe && hello_world.exe
+CALL cargo run hello_world.rs
 POPD
 
 PUSHD python
 ECHO '-- hello_world.py'
-python hello_world.py
+CALL python hello_world.py
 POPD
 
 PUSHD csharp
 ECHO '-- hello_world.cs'
-dotnet run
+CALL dotnet run
 POPD
 
 PUSHD fsharp
 ECHO '-- hello_world.fs'
-dotnet run
+CALL dotnet run
 POPD
 
 PUSHD java
 ECHO '-- hello_world.java'
 REM javac HelloWorld.java && java HelloWorld
-java HelloWorld.java
+CALL java HelloWorld.java
 POPD
 
 PUSHD go
 ECHO '-- hello_world.go'
-go run hello_world.go
+CALL go run hello_world.go
 POPD
 
 @REM PUSHD haskell
 @REM ECHO '-- hello_world.hs'
-@REM ghc helloworld.hs && helloworld.exe
+@REM CALL ghc helloworld.hs && helloworld.exe
 @REM POPD
