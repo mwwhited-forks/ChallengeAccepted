53cf571dc463e82a83096a6c9ac14d9e5245d809
Matthew Whited <matt@whited.us>
2025-06-19 09:29:38 -0400

    update generator

--- src/HammingGenerator/csharp/HammingGenerator.Cli/Program.cs
+++ src/HammingGenerator/csharp/HammingGenerator.Cli/Program.cs
@@ -7,85 +7,94 @@ internal class Program
 
     static void Main(string[] args)
     {
-        uint bitdepth = 8;
-        uint mask = (uint)Math.Pow(2, bitdepth) - 1;
-
-        var masks = new List<(uint mask, uint distance)>();
-        masks.Add((0, 0));
-
-        var distance = bitdepth;
-
-        //Console.WriteLine($"mask\tb:{Convert.ToString(mask, 2).PadLeft(15, '0')}");
-        for (uint d = 0; d < bitdepth; d++)
+        uint[] bitDepths = [5, 8, 10, 12, 16];
+        foreach (var bitDepth in bitDepths)
         {
-            var c = Fact(bitdepth) / (Fact(d) * Fact((uint)(bitdepth - d)));
-            Console.WriteLine($"{d}\t{c}");
+            Console.WriteLine($"Bits: {bitDepth}");
+            GenerateHammingDistanceData(bitDepth);
         }
 
-        var set = new List<uint>();
+        static void GenerateHammingDistanceData(uint bitdepth)
+        {
+            uint mask = (uint)Math.Pow(2, bitdepth) - 1;
 
-        for (uint b = 0; b < bitdepth; b++)
-        {
-            uint v =(uint)(1 << (int)b);
-            set.Add(v);
-        }
-        var idx = 0;
-        foreach (var s in set)
-        {
-            // Console.WriteLine($"{1}-{Convert.ToString(s, 2).PadLeft(15, '0')}-({s:000})-{idx++:00}");
-            masks.Add((s, 1));
-        }
+            var masks = new List<(uint mask, uint distance)>();
+            masks.Add((0, 0));
 
-        for (uint d = 1; d < distance; d++)
-        {
-            var set2 = new List<uint>();
-            foreach (var s in set.Distinct())
+            var distance = bitdepth;
+
+            //Console.WriteLine($"mask\tb:{Convert.ToString(mask, 2).PadLeft(15, '0')}");
+            for (uint d = 0; d < bitdepth; d++)
             {
-                uint v = (s << 1) + 1;
-                for (var x = 0; x <= bitdepth - d; x++)
+                var c = Fact(bitdepth) / (Fact(d) * Fact((uint)(bitdepth - d)));
+                Console.WriteLine($"{d}\t{c}");
+            }
+
+            var set = new List<uint>();
+
+            for (uint b = 0; b < bitdepth; b++)
+            {
+                uint v = (uint)(1 << (int)b);
+                set.Add(v);
+            }
+            var idx = 0;
+            foreach (var s in set)
+            {
+                // Console.WriteLine($"{1}-{Convert.ToString(s, 2).PadLeft(15, '0')}-({s:000})-{idx++:00}");
+                masks.Add((s, 1));
+            }
+
+            for (uint d = 1; d < distance; d++)
+            {
+                var set2 = new List<uint>();
+                foreach (var s in set.Distinct())
                 {
-                    var v2 = v << x;
-                    if (v2 < mask)
-                        set2.Add(v2 & mask);
+                    uint v = (s << 1) + 1;
+                    for (var x = 0; x <= bitdepth - d; x++)
+                    {
+                        var v2 = v << x;
+                        if (v2 < mask)
+                            set2.Add(v2 & mask);
+                    }
+                }
+                set.Clear();
+                set.AddRange(set2);
+                set2.Clear();
+
+                idx = 0;
+                foreach (var s in set.Distinct().Order())
+                {
+                    //Console.WriteLine($"{d+1}-{Convert.ToString(s, 2).PadLeft(15, '0')}-({s:000})-{idx++:00}");
+                    masks.Add((s, (uint)d + 1));
                 }
             }
-            set.Clear();
-            set.AddRange(set2);
-            set2.Clear();
+            masks.Add((mask, bitdepth));
 
-            idx = 0;
-            foreach (var s in set.Distinct().Order())
+            foreach (var s in masks)
             {
-                //Console.WriteLine($"{d+1}-{Convert.ToString(s, 2).PadLeft(15, '0')}-({s:000})-{idx++:00}");
-                masks.Add((s, (uint)d + 1));
+                Console.WriteLine($"{s.distance}-{Convert.ToString(s.mask, 2).PadLeft((int)bitdepth, '0')}");
             }
-        }
-        masks.Add((mask, bitdepth));
 
-        foreach (var s in masks)
-        {
-            Console.WriteLine($"{s.distance}-{Convert.ToString(s.mask, 2).PadLeft((int)bitdepth, '0')}");
-        }
+            File.WriteAllLines($"Hamming{bitdepth}.csv",
+                from m in masks
+                select $"{Convert.ToString(m.mask, 2).PadLeft((int)bitdepth, '0')},{m.distance:00},{m.mask:00000}"
+                       );
 
-        File.WriteAllLines($"Hamming{bitdepth}.csv",
-            from m in masks
-            select $"{m.distance},{m.mask},{Convert.ToString(m.mask, 2).PadLeft((int)bitdepth, '0')}"
-                   );
+            var r = new Random();
+            var needle = (byte)(r.Next() & 0xff);
 
-        var r = new Random();
-        var needle = (byte)(r.Next() & 0xff);
+            var haystack = from m in masks
+                           select new
+                           {
+                               m.distance,
+                               m.mask,
+                               match = m.mask ^ needle
+                           };
 
-        var haystack = from m in masks
-                       select new
-                       {
-                           m.distance,
-                           m.mask,
-                           match = m.mask ^ needle
-                       };
-
-        foreach (var s in haystack)
-        {
-            Console.WriteLine(s);
+            foreach (var s in haystack)
+            {
+                Console.WriteLine(s);
+            }
         }
     }
 }
